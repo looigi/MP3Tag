8f8977724e19f54af44bd77a04ce7f66a12147b8
Luigi Pecce <looigi@gmail.com>
2019-07-26 13:37:41 +0200

    Risolti problemi di download file

--- MP3Tag/App_Code/Download.vb
+++ MP3Tag/App_Code/Download.vb
@@ -2,154 +2,157 @@
 Imports System.Net
 Imports System.Text
 Imports System.ComponentModel
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Net.Security
 
 Public Class Download
-    Private TipoCollegamento As String
-    Private Utenza As String
-    Private Password As String
-    Private Dominio As String
-    Private sNomeFileConPercorso As String
+	Private TipoCollegamento As String
+	Private Utenza As String
+	Private Password As String
+	Private Dominio As String
+	Private sNomeFileConPercorso As String
 
-    Public Sub ImpostaValori(Tc As String, U As String, P As String, D As String, nf As String)
-        TipoCollegamento = Tc
-        Utenza = U
-        Password = P
-        Dominio = D
-        sNomeFileConPercorso = nf
-    End Sub
+	Public Sub ImpostaValori(Tc As String, U As String, P As String, D As String, nf As String)
+		TipoCollegamento = Tc
+		Utenza = U
+		Password = P
+		Dominio = D
+		sNomeFileConPercorso = nf
+	End Sub
 
-    Public Sub CreaEPulisceCartellaDiLavoro()
-        Dim gf As New GestioneFilesDirectory
-        gf.CreaDirectoryDaPercorso(gf.TornaNomeDirectoryDaPath(sNomeFileConPercorso) & "\")
+	Public Sub CreaEPulisceCartellaDiLavoro()
+		Dim gf As New GestioneFilesDirectory
+		gf.CreaDirectoryDaPercorso(gf.TornaNomeDirectoryDaPath(sNomeFileConPercorso) & "\")
 
-        gf.ScansionaDirectorySingola(gf.TornaNomeDirectoryDaPath(sNomeFileConPercorso) & "\")
-        Dim Filetti() As String = gf.RitornaFilesRilevati
-        Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
-        For i As Integer = 1 To qFiles
-            If Filetti(i).ToUpper.Contains(".XML") Then
-                gf.EliminaFileFisico(Filetti(i))
-            End If
-        Next
+		gf.ScansionaDirectorySingola(gf.TornaNomeDirectoryDaPath(sNomeFileConPercorso) & "\")
+		Dim Filetti() As String = gf.RitornaFilesRilevati
+		Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
+		For i As Integer = 1 To qFiles
+			If Filetti(i).ToUpper.Contains(".XML") Then
+				gf.EliminaFileFisico(Filetti(i))
+			End If
+		Next
 
-        If File.Exists(sNomeFileConPercorso) = True Then
-            Dim Conta As Integer = 1
-            Dim Estensione As String = gf.TornaEstensioneFileDaPath(sNomeFileConPercorso)
+		If File.Exists(sNomeFileConPercorso) = True Then
+			Dim Conta As Integer = 1
+			Dim Estensione As String = gf.TornaEstensioneFileDaPath(sNomeFileConPercorso)
 
-            sNomeFileConPercorso = sNomeFileConPercorso.Replace(Estensione, "")
+			sNomeFileConPercorso = sNomeFileConPercorso.Replace(Estensione, "")
 
-            Do While File.Exists(sNomeFileConPercorso & Format(Conta, "00") & Estensione) = True
-                Conta += 1
-            Loop
+			Do While File.Exists(sNomeFileConPercorso & Format(Conta, "00") & Estensione) = True
+				Conta += 1
+			Loop
 
-            sNomeFileConPercorso = sNomeFileConPercorso & Format(Conta, "00") & Estensione
-        End If
-    End Sub
+			sNomeFileConPercorso = sNomeFileConPercorso & Format(Conta, "00") & Estensione
+		End If
+	End Sub
 
-    Public Function ScaricaPagina(Url As String) As Boolean
-        Dim Ok As Boolean = True
-        Dim sourceCode As String
-        Dim gf As New GestioneFilesDirectory
+	Public Function ScaricaPagina(Url As String) As Boolean
+		Dim Ok As Boolean = True
+		Dim sourceCode As String
+		Dim gf As New GestioneFilesDirectory
 
-        If TipoCollegamento Is Nothing = True Then TipoCollegamento = ""
+		If TipoCollegamento Is Nothing = True Then TipoCollegamento = ""
 
-        If TipoCollegamento.Trim.ToUpper = "PROXY" Then
-            Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create(Url)
-            request.Proxy.Credentials = New System.Net.NetworkCredential(Utenza, Password, Dominio)
-            request.Timeout = 7000
-            Try
-                Dim response As System.Net.HttpWebResponse = request.GetResponse()
-                'Application.DoEvents()
-                Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
-                'Application.DoEvents()
-                sourceCode = sr.ReadToEnd()
-                sr.Close()
-                response.Close()
-                request = Nothing
+		If TipoCollegamento.Trim.ToUpper = "PROXY" Then
+			Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create(Url)
+			request.Proxy.Credentials = New System.Net.NetworkCredential(Utenza, Password, Dominio)
+			request.Timeout = 7000
+			Try
+				Dim response As System.Net.HttpWebResponse = request.GetResponse()
+				'Application.DoEvents()
+				Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+				'Application.DoEvents()
+				sourceCode = sr.ReadToEnd()
+				sr.Close()
+				response.Close()
+				request = Nothing
 
-                gf.CreaAggiornaFile(sNomeFileConPercorso, sourceCode)
-            Catch ex As Exception
-                Ok = False
-            End Try
-        Else
-            Try
-                'Dim wreq As WebRequest = WebRequest.Create(Url)
-                'Dim wres As WebResponse = wreq.GetResponse()
-                'Dim iBuffer As Integer = 0
-                'Dim buffer(128) As [Byte]
-                'Dim stream As Stream = wres.GetResponseStream()
-                'iBuffer = stream.Read(buffer, 0, 128)
-                'Dim strRes As New StringBuilder("")
-                'While iBuffer <> 0
-                '    strRes.Append(Encoding.ASCII.GetString(buffer, 0, iBuffer))
-                '    iBuffer = stream.Read(buffer, 0, 128)
-                'End While
-                'gf.CreaAggiornaFile(sNomeFileConPercorso, strRes.ToString())
+				gf.CreaAggiornaFile(sNomeFileConPercorso, sourceCode)
+			Catch ex As Exception
+				Ok = False
+			End Try
+		Else
+			Try
+				'Dim wreq As WebRequest = WebRequest.Create(Url)
+				'Dim wres As WebResponse = wreq.GetResponse()
+				'Dim iBuffer As Integer = 0
+				'Dim buffer(128) As [Byte]
+				'Dim stream As Stream = wres.GetResponseStream()
+				'iBuffer = stream.Read(buffer, 0, 128)
+				'Dim strRes As New StringBuilder("")
+				'While iBuffer <> 0
+				'    strRes.Append(Encoding.ASCII.GetString(buffer, 0, iBuffer))
+				'    iBuffer = stream.Read(buffer, 0, 128)
+				'End While
+				'gf.CreaAggiornaFile(sNomeFileConPercorso, strRes.ToString())
 
-                ScaricaFileAsincrono(Url, sNomeFileConPercorso)
-            Catch ex As Exception
-                Ok = False
-            End Try
-        End If
+				ScaricaFileAsincrono(Url, sNomeFileConPercorso)
+			Catch ex As Exception
+				'MsgBox(ex.Message)
+				Ok = False
+			End Try
+		End If
 
-        Return Ok
-    End Function
+		Return Ok
+	End Function
 
-    Public Function ScaricaFile(Url As String, Optional sNomeDestinazione As String = "") As Boolean
-        Dim Ok As Boolean = True
-        ' Dim sourceCode As String
-        If sNomeDestinazione = "" Then sNomeDestinazione = sNomeFileConPercorso
-        Dim gf As New GestioneFilesDirectory
+	Public Function ScaricaFile(Url As String, Optional sNomeDestinazione As String = "") As Boolean
+		Dim Ok As Boolean = True
+		' Dim sourceCode As String
+		If sNomeDestinazione = "" Then sNomeDestinazione = sNomeFileConPercorso
+		Dim gf As New GestioneFilesDirectory
 
-        If TipoCollegamento Is Nothing = True Then TipoCollegamento = ""
+		If TipoCollegamento Is Nothing = True Then TipoCollegamento = ""
 
-        If TipoCollegamento.Trim.ToUpper = "PROXY" Then
-            Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create(Url)
-            request.Proxy.Credentials = New System.Net.NetworkCredential(Utenza, Password, Dominio)
-            Dim response As System.Net.HttpWebResponse = request.GetResponse()
-            'Application.DoEvents()
+		If TipoCollegamento.Trim.ToUpper = "PROXY" Then
+			Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create(Url)
+			request.Proxy.Credentials = New System.Net.NetworkCredential(Utenza, Password, Dominio)
+			Dim response As System.Net.HttpWebResponse = request.GetResponse()
+			'Application.DoEvents()
 
-            Dim responseStream As Stream = response.GetResponseStream()
-            Dim imageBytes() As Byte
+			Dim responseStream As Stream = response.GetResponseStream()
+			Dim imageBytes() As Byte
 
-            Using br As New BinaryReader(responseStream)
-                imageBytes = br.ReadBytes(500000)
-                br.Close()
-            End Using
-            responseStream.Close()
-            response.Close()
+			Using br As New BinaryReader(responseStream)
+				imageBytes = br.ReadBytes(500000)
+				br.Close()
+			End Using
+			responseStream.Close()
+			response.Close()
 
-            Dim fs As New FileStream(sNomeFileConPercorso, FileMode.Create)
-            Dim bw As New BinaryWriter(fs)
-            Try
-                bw.Write(imageBytes)
-            Finally
-                fs.Close()
-                bw.Close()
-            End Try
+			Dim fs As New FileStream(sNomeFileConPercorso, FileMode.Create)
+			Dim bw As New BinaryWriter(fs)
+			Try
+				bw.Write(imageBytes)
+			Finally
+				fs.Close()
+				bw.Close()
+			End Try
 
-            request = Nothing
+			request = Nothing
 
-            response.Close()
-            response = Nothing
-            request = Nothing
-        Else
-            Dim myWebClient As New WebClient()
+			response.Close()
+			response = Nothing
+			request = Nothing
+		Else
+			Dim myWebClient As New WebClient()
 
-            Try
-                myWebClient.DownloadFile(
-                            Url,
-                            sNomeDestinazione)
-            Catch ex As Exception
+			Try
+				myWebClient.DownloadFile(
+							Url,
+							sNomeDestinazione)
+			Catch ex As Exception
 
-            End Try
-        End If
+			End Try
+		End If
 
-        Return Ok
-    End Function
+		Return Ok
+	End Function
 
-    Private StaScaricando As Boolean
-    'Private pbDown As ProgressBar = Nothing
-    Private Blocca As Boolean
+	Private StaScaricando As Boolean
+	'Private pbDown As ProgressBar = Nothing
+	Private Blocca As Boolean
     Private wb As New WebClient
     Private sNomeFiletto As String
     Private Inizio As Date
@@ -161,22 +164,24 @@ Public Class Download
 
         'Try
         Dim Uri As New Uri(Url)
-        'AddHandler wb.DownloadFileCompleted, AddressOf Completed
-        'AddHandler wb.DownloadProgressChanged, AddressOf Scaricando
+		'AddHandler wb.DownloadFileCompleted, AddressOf Completed
+		'AddHandler wb.DownloadProgressChanged, AddressOf Scaricando
 
-        'If pbDownload Is Nothing = False Then
-        '    pbDownload.Value = 0
-        '    pbDownload.Visible = True
+		'If pbDownload Is Nothing = False Then
+		'    pbDownload.Value = 0
+		'    pbDownload.Visible = True
 
-        '    pbDown = pbDownload
-        'End If
+		'    pbDown = pbDownload
+		'End If
 
-        sNomeFiletto = sNomeFile
-        wb.DownloadFile(Uri, sNomeFile)
-        'Catch ex As Exception
+		sNomeFiletto = sNomeFile
 
-        'End Try
-    End Sub
+		ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+		wb.DownloadFile(Uri, sNomeFile)
+		'Catch ex As Exception
+
+		'End Try
+	End Sub
 
     Public Sub BloccaElaborazione()
         Blocca = True
